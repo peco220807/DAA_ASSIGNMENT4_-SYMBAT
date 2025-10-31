e86a1c2497e485f7eb06ffc8bc8aec2086c9037d
peco220807 <saparbai.s07@icloud.com>
2025-10-31 17:26:16 +0500

    adding classes: Edge and Graph

--- src/main/java/graph/Graph.java
+++ src/main/java/graph/Graph.java
@@ -1,4 +1,25 @@
 package graph;
+import java.util.ArrayList;
+import java.util.List;
 public class Graph {
-
+    private final int n;
+    private final boolean directed;
+    private final List<Edge>[] adj;
+    public Graph(int n, boolean directed) {
+        this.n = n;
+        this.directed = directed;
+        adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+    }
+    public void addEdge(int u, int v, int w) {
+        adj[u].add(new Edge(u, v, w));
+        if (!directed) adj[v].add(new Edge(v, u, w));
+    }
+    public List<Edge> getAdj(int u) {
+        return adj[u];
+    }
+    public int size() {
+        return n;
+    }
 }
+
