5472d545d15cf3d5f0d7e11817459ed21cb6430c
peco220807 <saparbai.s07@icloud.com>
2025-10-31 17:50:15 +0500

    adding TopologicalSort.java

--- src/main/java/graph/topo/TopologicalSort.java
+++ src/main/java/graph/topo/TopologicalSort.java
@@ -3,5 +3,29 @@ import graph.Graph;
 import graph.Edge;
 import java.util.*;
 public class TopologicalSort {
-
+    private final Graph dag;
+    public TopologicalSort(Graph dag) {
+        this.dag = dag;
+    }
+    public List<Integer> sort() {
+        int n = dag.size();
+        int[] inDegree = new int[n];
+        for (int v = 0; v < n; v++) {
+            for (Edge e : dag.getAdj(v)) {
+                inDegree[e.to]++;
+            }
+        }
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < n; i++) if (inDegree[i] == 0) queue.add(i);
+        List<Integer> order = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            order.add(v);
+            for (Edge e : dag.getAdj(v)) {
+                inDegree[e.to]--;
+                if (inDegree[e.to] == 0) queue.add(e.to);
+            }
+        }
+        return order;
+    }
 }
